46839bd4562548fa83074d2227287cb6e6ad42bf
Bushsbakedbean <114026987+Bushsbakedbean@users.noreply.github.com>
2023-07-28 16:05:34 -0400

    Added a conversion function for absolute encoders

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -139,4 +139,11 @@ public final class Constants {
   public static final class NeoMotorConstants {
     public static final double kFreeSpeedRpm = 5676;
   }
+
+  public static final double AbsoluteEncoderToDegrees(double number, double offset) {
+
+    return (number * 384.6153) + offset;
+
+  }
+
 }
